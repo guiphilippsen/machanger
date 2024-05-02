6049be2ada32e03dc4507a943b8d7ce85cc92072
Guilherme Philippsen de Souza <guilhermephilippsen2003@gmail.com>
2024-05-02 19:49:49 -0300

    teste

--- main.py
+++ main.py
@@ -7,11 +7,6 @@ print("1 - Show Ethernet Interfaces\n2 - Change MAC Addressg\n0 - Close Program"
 trigger = input("Select an option: ")
 
 
-def generate_mac_address():
-    mac = [random.randint(0x00, 0xFF) for _ in range(6)]
-    return ':'.join(map(lambda x: f'{x:02x}', mac))
-
-
 if trigger == '0':
     print("Closing Program...")
 elif trigger == '1':
@@ -20,6 +15,9 @@ elif trigger == '1':
     subprocess.call("ifconfig")
 elif trigger == '2':
     interface = input("Select your Enternet Interface: ")
+    def generate_mac_address():
+        mac = [random.randint(0x00, 0xFF) for _ in range(6)]
+        return ':'.join(map(lambda x: f'{x:02x}', mac))
     print("Generating your new MAC Address...")
     time.sleep(1)
     subprocess.call("ifconfig " + interface + " down")
